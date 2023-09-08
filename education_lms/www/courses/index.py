61a015196a427b6d2285d1ad902f956062c23262
Omar <omaralhoori@gmail.com>
2023-09-08 18:26:19 +0300

    postponed semester dont show courses

--- education_lms/www/courses/index.py
+++ education_lms/www/courses/index.py
@@ -1,3 +1,4 @@
+from education.education.doctype.study_postponement.study_postponement import check_postponed_semester
 from education_lms.education_lms.utils import get_enrolled_courses, get_live_courses
 import frappe
 from frappe import _
@@ -6,6 +7,10 @@ from frappe import _
 def get_context(context):
 	context.no_cache = 1
 	context.show_sidebar = True
+	student = frappe.db.get_value("Student", {"user": frappe.session.user}, "name")
+	context.is_semester_avilable = check_postponed_semester(student)
+	if not context.is_semester_avilable:
+		return context
 	context.enrolled_courses = get_enrolled_courses()
 	context.show_live_courses = frappe.db.get_single_value("LMS Settings", 'show_live_courses')
 	if context.show_live_courses:
